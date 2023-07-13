355bde9bc9b696c15ebfc7b80523a212280f892d
Shubham <shubhamrai@thoughtwin.com>
2023-07-13 19:27:56 +0530

    userregister

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,101 +1,131 @@
-import React from "react";
-
-import { Box, Button, Container, TextField, Typography } from "@mui/material";
-
-import Link from "next/link";
-import Image from "next/image";
-import img from "../Home/assets/amish-thakkar-1rLF6hxmm7M-unsplash.jpg";
+import React, { use, useEffect, useState } from "react";
+import "./login.css";
+import Signup from "../Signup/Signup";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const Login = () => {
-  return (
-    <Box>
-      <Container
-        sx={{
-          background: "white",
-          width: "80%",
-          margin: "auto",
-          height: "70vh",
-          display: "flex",
-          justifyContent: "space-between",
-          p: 3,
-          gap: "20px",
-        }}
-      >
-        <Box>
-          <Box sx={{ background: "red", width: "400px", height: "50vh" }}>
-            <Image src={img.src} alt="img" width={400} height={400}></Image>
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            width: "50%",
-            margin: "auto",
-          }}
-        >
-          <Typography>Enter Credentials to login</Typography>
-          <Box
-            component="form"
-            sx={{
-              "& > :not(style)": { m: 1, width: "auto" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField id="outlined-basic" label="Email" variant="outlined" />
-          </Box>
+  // const router = useRouter();
+  const [signUpDetails, setSignupDetails] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [LoginDetails, setLoginDetails] = useState({
+    email: "",
+    password: "",
+  });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
-          <Box
-            component="form"
-            sx={{
-              "& > :not(style)": { m: 1, width: "auto" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField
-              id="outlined-basic"
-              label="Password"
-              variant="outlined"
+  const handleSignUpInput = (e: any) => {
+    setSignupDetails({ ...signUpDetails, [e.target.name]: e.target.value });
+  };
+  const handleLoginInput = (e: any) => {
+    setLoginDetails({ ...LoginDetails, [e.target.name]: e.target.value });
+  };
+  const handleSignUpCss = () => {
+    function handle() {
+      document.querySelector<any>(".cont").classList.toggle("s--signup");
+    }
+
+    handle();
+  };
+
+  useEffect(() => {
+    if (
+      (signUpDetails.username.length > 0 &&
+        signUpDetails.password.length > 0 &&
+        signUpDetails.email,
+      length > 0)
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [signUpDetails]);
+
+  const handleSignUp = async (e: any) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/users/signup", signUpDetails);
+
+      console.log("signup succcess", res.data);
+    } catch (error: any) {
+      toast.error(error.message);
+      console.log("sgnup failed", error);
+    }
+  };
+  const handleLogin = () => {};
+  return (
+    <div>
+      <div className="cont">
+        <div className="form sign-in">
+          <h2>Welcome</h2>
+          {/* <h3 style={{ color: "coral" }}>
+            Please Login for better Experience !!
+          </h3> */}
+          <label>
+            <span>Email</span>
+
+            <input type="email" name="email" onChange={handleLoginInput} />
+          </label>
+          <label>
+            <span>Password</span>
+            <input
+              type="password"
+              name="password"
+              onChange={handleLoginInput}
             />
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              gap: "20px",
-              flexDirection: "row !important",
-            }}
-          >
-            <Box>
-              {" "}
-              <Button
-                sx={{
-                  width: "auto",
-                  margin: "auto auto auto 20px",
-                  alignItems: "center",
-                }}
-                variant="contained"
-              >
-                Submit
-              </Button>
-            </Box>
-            <Box>
-              <Link href="/userprofileregistration">
-                <Button
-                  sx={{
-                    width: "auto",
-                    margin: "auto auto auto 50px",
-                    alignItems: "center",
-                  }}
-                  variant="contained"
-                >
-                  SignUp
-                </Button>
-              </Link>
-            </Box>
-          </Box>
-        </Box>
-      </Container>
-    </Box>
+          </label>
+          <p className="forgot-pass">Forgot password?</p>
+          <button className="button submit" type="button" onClick={handleLogin}>
+            Sign In
+          </button>
+        </div>
+        <div className="sub-cont">
+          <div className="img">
+            <div className="img__text m--up">
+              <h3>Don't have an account? Please Sign up!</h3>
+            </div>
+            <div className="img__text m--in">
+              <h3>If you already has an account, just sign in.</h3>
+            </div>
+            <div className="img__btn" onClick={handleSignUpCss}>
+              <span className="m--up">Sign Up</span>
+              <span className="m--in">Sign In</span>
+            </div>
+          </div>
+          <div className="form sign-up">
+            <h2>Create your Account</h2>
+            {/* <Signup /> */}
+            <label>
+              <span>Name</span>
+              <input type="text" name="username" onChange={handleSignUpInput} />
+            </label>
+            <label>
+              <span>Email</span>
+              <input type="email" name="email" onChange={handleSignUpInput} />
+            </label>
+            <label>
+              <span>Password</span>
+              <input
+                type="password"
+                name="password"
+                onChange={handleSignUpInput}
+              />
+            </label>
+            <button
+              type="button"
+              className="button submit"
+              onClick={handleSignUp}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
---
+++ src/dbConfig/dbconfig.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+export async function connect() {
+  try {
+    mongoose.connect("mongodb+srv://Shubh:12345@cluster1.jqjvz1t.mongodb.net/");
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("mongodb connected successfully");
+    });
+
+    connection.on("error", (err) => {
+      console.log("MongoDb connection error", err);
+    });
+  } catch (error) {
+    console.log("Something gone wrong", error);
+  }
+}
